c81d927e700c9b9145ee32c203e969e8262ff93e
Valancinis <dovydas.valancius@gmail.com>
2023-12-23 14:49:32 +0200

    Scrolling now works

--- bullet_rpa/main.py
+++ bullet_rpa/main.py
@@ -1,12 +1,7 @@
-import flet as ft
 from core import workers as workers, long_button
-import threading
 
-class State:
-    i = 0
+import flet as ft
 
-s = State()
-sem = threading.Semaphore()
 
 def main(page: ft.Page):
     # Defining a function that picks a file and updates the text field
@@ -93,39 +88,17 @@ def main(page: ft.Page):
             ft.ElevatedButton('-', style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=5)),
                               on_click=lambda e, index=i: remove_robot(index))
         ]))
-    bot_library = ft.Column(widgets)
+
+    bot_library = ft.Column(widgets,
+                            spacing=10,
+                            height=200,
+                            width=200,
+                            scroll=ft.ScrollMode.ALWAYS,
+                            on_scroll_interval=0
+                            )
 
     # Add controls to the page
-    page.add(header, input_name, file_pick_row, add_button, ft.Divider(), bot_library)
-
-
-
-    # Scrolling functionality
-    def on_scroll(e: ft.OnScrollEvent):
-        if e.pixels >= e.max_scroll_extent - 100:
-            if sem.acquire(blocking=False):
-                try:
-                    for i in range(0, 10):
-                        cl.controls.append(ft.Text(f"Text line {s.i}", key=str(s.i)))
-                        s.i += 1
-                    cl.update()
-                finally:
-                    sem.release()
-
-    cl = ft.Column(
-        spacing=10,
-        height=200,
-        width=200,
-        scroll=ft.ScrollMode.ALWAYS,
-        on_scroll_interval=0,
-        on_scroll=on_scroll,
-    )
-    for i in range(0, 50):
-        cl.controls.append(ft.Text(f"Text line {s.i}", key=str(s.i)))
-        s.i += 1
-
-    page.add(ft.Container(cl, border=ft.border.all(1)))
-    # End of scrolling functionality
+    page.add(header, input_name, file_pick_row, add_button, ft.Divider(), bot_library,)
 
 
 if __name__ == '__main__':
